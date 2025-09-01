a15d3cb25f7da1933faef83ecc2a4c29a0b30c36
ShenSingh <gayashan9090@gmail.com>
2025-09-02 03:28:26 +0530

    add readme file:project

--- src/utils/firebase-config.js
+++ src/utils/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaHFMynicAdachWfLyb7IAscnIM7PQmH4",
-  authDomain: "react-netflix-clone-fcb82.firebaseapp.com",
-  projectId: "react-netflix-clone-fcb82",
-  storageBucket: "react-netflix-clone-fcb82.appspot.com",
-  messagingSenderId: "441180316943",
-  appId: "1:441180316943:web:c816afffa3bb9791a5ca7a",
-  measurementId: "G-YGHM2XTEKZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
